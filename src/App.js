45be6ef726f4b2668ae8fe2f17664c966029152d
Roger Paris <roger.parisvicent@gmail.com>
2020-07-31 11:22:11 +0200

    adds firebase auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, Text, SafeAreaView, Button} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const App = () => {
-  return <View style={styles.container}></View>;
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  });
+
+  const onAuthStateChanged = (currentUser) => {
+    setUser(currentUser);
+    if (initializing) {
+      setInitializing(false);
+    }
+  };
+
+  const onPressLogout = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
+
+  if (initializing) {
+    return null;
+  }
+
+  if (!user) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>Login</Text>
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Welcome {user.email}</Text>
+      <Button title={'LOGOUT'} onPress={onPressLogout} />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
